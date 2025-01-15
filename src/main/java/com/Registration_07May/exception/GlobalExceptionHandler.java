757aca4e1b379c2091a685fd77729393aad1b627
rakeshshit <iamrakeshit60@gmail.com>
2025-01-16 01:57:21 +0530

    127 : error msg print with date, url

--- src/main/java/com/Registration_07May/exception/GlobalExceptionHandler.java
+++ src/main/java/com/Registration_07May/exception/GlobalExceptionHandler.java
@@ -1,16 +1,23 @@
 package com.Registration_07May.exception;
 
+import com.Registration_07May.payload.ErrorDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> resourceNotFound(ResourceNotFoundException r){
-        return new ResponseEntity<>(r.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<ErrorDto> resourceNotFound(ResourceNotFoundException r, WebRequest request){
+        ErrorDto error = new ErrorDto(r.getMessage(),new Date(),request.getDescription(false)); /* If id use true at that time url print with
+                            clint information like "uri": "uri=/api/v1/registration/10;client=0:0:0:0:0:0:0:1"
+                            > If I use FALSE at that time url like "uri": "uri": "uri=/api/v1/registration/10" */
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(Exception.class)
